f347c7723d7e72730c68cd9cae08d389271eb924
jc <jc@pop-os.localdomain>
2021-07-20 22:45:36 +0200

    add email validaton

--- models/contact.js
+++ models/contact.js
@@ -1,10 +1,20 @@
 var mongoose = require('mongoose')
 
+const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 var ContactSchema = new mongoose.Schema({
     name: { type: String, required: true },
     user: { type: String, required: true },
     timestamp: { type: Date, required: true },
-    email: { type: String, required: true },
+    email: {
+        type: String, required: true,
+        validate: {
+            message: "Provide a valid email",
+            validator(email) {
+                return EMAIL_REGEX.test(email.toLowerCase());
+            }
+        }
+    },
     message: { type: String, required: false },
 })
 
