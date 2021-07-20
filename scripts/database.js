a4460697a10c2c287e290099401a9e85b5c39724
jc <jc@pop-os.localdomain>
2021-07-20 22:49:56 +0200

    refactor operations

--- scripts/database.js
+++ scripts/database.js
@@ -1,34 +1,55 @@
 const http = require('./http')
 
 module.exports = {
-    create(model, response, object) {
-        model.create(object, (error, created) => http.handleCreated(response, error, created))
+    create(model, response, object, callback) {
+        model.create(object, (error, created) => {
+            if (error) http.BadRequest(response, error)
+            else {
+                if (callback) callback(created)
+                http.OK(response, "Successfully created", created)
+            }
+        })
     },
     readOne(model, response, query) {
         model.findOne(query, (error, object) => {
-            if (object) http.handleJson(response, error, object)
+            if (error) http.BadRequest(response, error)
+            else if (object) response.json(object)
             else http.NotFound(response)
         })
     },
     readAll(model, response, query) {
-        model.find(query, (error, objects) => http.handleJson(response, error, objects))
+        model.find(query, (error, objects) => {
+            if (error) http.BadRequest(response, error)
+            else response.json(objects)
+        })
     },
     readAllAsObject(model, response) {
-        model.find((error, objects) => http.handleJson(response, error, objects.map(o => o.toObject())))
+        model.find((error, objects) => {
+            if (error) http.BadRequest(response, error)
+            else response.json(objects.map(o => o.toObject()))
+        })
     },
     update(model, response, query, updated) {
         model.findOneAndUpdate(query, updated, { new: true },
-            (error, updated) => http.handleUpdated(response, error, updated)
+            (error, updated) => {
+                if (error) http.BadRequest(response, error)
+                else if (updated) http.OK(response, "Successfully updated", updated)
+                else http.NotFound(response)
+            }
         )
     },
     deleteOne(model, response, query, payload) {
         model.deleteOne(query, (error, result) => {
-            http.handleDeleted(response, error, result, payload)
+            if (error) http.BadRequest(response, error)
+            else if (result.deletedCount == 0) http.NotFound(response)
+            else http.OK(response, "Successfully deleted", payload)
         });
     },
     deleteMany(model, response, query, payload) {
         model.deleteMany(query, (error, result) => {
-            http.handleDeleted(response, error, result, payload)
+            if (error) http.BadRequest(response, error)
+            else if (result.deletedCount == 0) http.NotFound(response)
+            else http.OK(response, "Successfully deleted", payload)
         });
     },
 }
