da8baba4ed14c36371e12ec89c25a46d068f8e43
jc <jc@pop-os.localdomain>
2021-07-20 22:50:33 +0200

    remove innecesary functions

--- scripts/http.js
+++ scripts/http.js
@@ -20,35 +20,6 @@ class Http {
         }
         response.status(400).send(payload)
     }
-    // Handlers
-    handleJson(response, error, json) {
-        if (error) this.BadRequest(response, error)
-        else response.json(json)
-    }
-    handleRead(response, error, obj) {
-        if (error) this.BadRequest(response, error)
-        else if (obj) response.json(obj)
-        else this.NotFound(response)
-    }
-    handleCreated(response, error, created) {
-        if (error) this.BadRequest(response, error)
-        else this.OK(response, "Successfully created", created)
-    }
-    handleUpdated(response, error, updated) {
-        if (error) this.BadRequest(response, error)
-        else if (updated) this.OK(response, "Successfully updated", updated)
-        else this.NotFound(response)
-    }
-    handleDeletedId(response, error, deleted) {
-        if (error) this.BadRequest(response, error)
-        else if (deleted) this.OK(response, "Successfully deleted", deleted)
-        else this.NotFound(response)
-    }
-    handleDeleted(response, error, result, payload) {
-        if (error) this.BadRequest(response, error)
-        else if (result.deletedCount == 0) this.NotFound(response)
-        else this.OK(response, "Successfully deleted", payload)
-    }
 }
 
 module.exports = new Http()
